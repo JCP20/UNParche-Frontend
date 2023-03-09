2f3a986b3f317d29c600450b084cabee731fccfd
Juliana Cardozo <cardozojuliana908@gmail.com>
2023-03-09 17:25:47 -0500

    Creación elementos del formulario de inicio de sesion. Atendiendo a las necesidades del usuario de ingresar en la plataforma, se creo el componente correspondiente al login, utilizando un formulario basado en Antd. Este componente incluye los encabezados y campos pertenecientes al contraseña y correo electrónico. En este punto están implementadas las verificaciones correspondientes a que los campos no se encuentren vacíos al momento de enviar el formulario. Adicionalmente se configuraron elementos graficos de la tarjeta y fondo. == impacto en stakeholders == == usuarios == Los usuarios podrán visualizar la página de inicio de sesion.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/styles.css";
+import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  return      <ConfigProvider
+  theme={{
+    token: {
+      colorPrimary: '#EB455F',
+      colorError: '#9D2503',
+    },
+  }}
+> <Component {...pageProps} />;
+</ConfigProvider>
 }
