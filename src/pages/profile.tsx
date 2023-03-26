8395e1a6f00e086101c80498648700a5938642b0
jvelozag-we <83142824+jvelozag-we@users.noreply.github.com>
2023-03-26 14:48:26 -0500

    creación de componente image_box y de página de profile

---
+++ src/pages/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from "../components/image_box"
+const profile = () => {
+  return (
+    <div>
+        <Box/>
+        </div>
+      
+  )
+}
+
+export default profile
