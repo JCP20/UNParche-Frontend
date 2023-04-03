29e3c6fdd68965dbd9611be02457b47dc6d0db37
jvelozag-we <83142824+jvelozag-we@users.noreply.github.com>
2023-04-02 20:05:11 -0500

    Delete de archivos
    Se borran archivos inecesarios

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import group_box from "../components/group_box"
+import Profileheader from "../components/profile_header"
 const profile = () => {
   return (
-    <div>
-        Hola
+
+        <div >
+          <Profileheader name={'Juliana Cardozo'} email={'jcardozo@unal.edu.co'}/>
         </div>
-      
+
   )
 }
 
