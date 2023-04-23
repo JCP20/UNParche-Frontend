3884e9e83bd0b235b6252c23bc118454a2e5cc89
Sergio Sanchez <chechoo.alejandro@gmail.com>
2023-04-23 13:00:14 -0500

    Creación de rama QA

--- src/context/auth/AuthProvider.tsx
+++ src/context/auth/AuthProvider.tsx
@@ -16,13 +16,21 @@ const AuthProvider = ({ children }: { children: JSX.Element }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [user, setUser] = useState<userAuthTypes>({} as userAuthTypes);
+  const [isTokenCheckCompleted, setIsTokenCheckCompleted] =
+    useState<boolean>(false);
 
   const router = useRouter();
 
-  const login = async (token: string, id: string, username: string) => {
+  const login = async (
+    token: string,
+    id: string,
+    username: string,
+    refresh: string
+  ) => {
     try {
       setIsAuthenticated(true);
       Cookies.set("x-token", token);
+      Cookies.set("x-token-refresh", refresh);
       setUser({ id, username });
       router.replace("/");
     } catch (error) {
@@ -34,6 +42,7 @@ const AuthProvider = ({ children }: { children: JSX.Element }) => {
     setUser({} as IUser);
     setIsAuthenticated(false);
     Cookies.remove("x-token");
+    Cookies.remove("x-token-refresh");
     router.replace("/login");
   };
 
@@ -46,35 +55,45 @@ const AuthProvider = ({ children }: { children: JSX.Element }) => {
         return;
       }
 
-      const resp: { ok: boolean; token: string; data: IUser } = (
-        await backendApi.get("/auth/renew")
-      ).data as { ok: boolean; token: string; data: IUser };
+      const resp = (await backendApi.get("/auth/renew")).data;
 
       if (resp.ok) {
+        Cookies.set("x-token", resp.token);
+        Cookies.set("x-token-refresh", resp.refresh);
         setUser(resp.data);
         setIsAuthenticated(true);
       } else {
         logout();
       }
     } catch (error) {
-      console.log(error);
+      logout();
+    } finally {
+      setIsTokenCheckCompleted(true);
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    // Don't check token on an array of defined public routes
+    // Don't check token on public routes
     setIsLoading(true);
+    setIsTokenCheckCompleted(false);
     if (privateRoutes.includes(router.pathname)) {
       checkAuthToken();
+    } else {
+      setIsLoading(false);
     }
-    setIsLoading(false);
   }, []);
 
-  if (isLoading) {
+  if (
+    isLoading ||
+    (!isAuthenticated &&
+      isTokenCheckCompleted &&
+      privateRoutes.includes(router.pathname))
+  ) {
     return (
       <div className="loadingComponent">
         <div className="containerNeonLoading">
-          <h2>Cargando...</h2>
+          <h2>Cargando</h2>
           <p>Por favor, espera un momento</p>
           <div className="bubbleContainer">
             <div className="bubble bubble-1" />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,9 +19,6 @@ export default function App({ Component, pageProps }: AppProps) {
           },
         }}
       >
-        <Head>
-          <link rel="icon" href="/imagenes/Logo_peque.jpeg" />
-        </Head>
         <Component {...pageProps} />
       </ConfigProvider>
     </AuthProvider>
