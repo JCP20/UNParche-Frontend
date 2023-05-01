d3977f156da5c457b781c25a4472d7615b7fa0e1
Sergio Sanchez <chechoo.alejandro@gmail.com>
2023-04-30 22:08:58 -0500

    Fix imports

--- src/pages/crearGrupo.tsx
+++ src/pages/crearGrupo.tsx
@@ -1,8 +1,8 @@
 import FormEvento from "@/components/FormEvent";
 import FormGrupo from "@/components/CreateGroup";
-import { createEventFn } from "@/services/events";
-import { createGroupFn, updateGroupFn } from "@/services/groups";
 import React from "react";
+import { createEventFn } from "@/services/events.service";
+import { createGroupFn, updateGroupFn } from "@/services/groups.service";
 
 const crearGrupo = () => {
   return (
