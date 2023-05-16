627cea769d72c68ebb3e9837e81a576607dc3db2
GabrielOtalora <gotalorag@unal.edu.co>
2023-05-16 09:21:14 -0500

    Integración barra de búsqueda  de usuarios

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { backendApiPrivate } from "@/services/api/config";
 import { IUser } from "@/interfaces/user";
 import { AxiosResponse } from "axios";
+import { backendApi } from "@/services/api/config";
 
 export const getUserById = async (id: string): Promise<IUser | null> => {
   try {
@@ -19,6 +20,17 @@ export const updateUserFn = async (
     const resp = await backendApiPrivate.put(`/users/${id}`, input);
     return resp;
   } catch (error) {
+    return null;
+  }
+};
+
+export const getUByParamFn = async (body: {
+  username: string
+}): Promise<AxiosResponse<any> | null> => {
+  try {
+    const result = await backendApi.post("/users/getbyparam/", body);
+    return result;
+  } catch (error) { 
     return null;
   }
 };
